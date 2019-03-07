52e91acd70a2734d2bf6a73923f464f25fd8b2cf
aprocyk <48320987+aprocyk@users.noreply.github.com>
2019-03-07 14:48:05 +0100

    Zgadywanka 0.1v
    Dodano wazne pytania

---
+++ GraZgadywanka/GraMonolit/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraMonolit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            string nazwa;
+            Console.Write("Napisz co: ");
+            nazwa = Console.ReadLine();
+            Console.WriteLine($"Napisales, {nazwa} bobie");
+            Console.WriteLine("Bye World");
+        }
+    }
+}
