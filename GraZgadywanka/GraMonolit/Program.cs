c3eb9837df31f9e56db6c22500d1aa1b692024f1
aprocyk <48320987+aprocyk@users.noreply.github.com>
2019-03-14 14:54:39 +0100

    Update Program.cs

--- GraZgadywanka/GraMonolit/Program.cs
+++ GraZgadywanka/GraMonolit/Program.cs
@@ -10,13 +10,50 @@ namespace GraMonolit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Gra za duzo za malo");
+            #region losowanie
+            //1. komputer losuje
+            Random generator = new Random();
+            int wylosowana = generator.Next(0, 101);
+#if DEBUG == true
+            //Console.WriteLine(wylosowana); //do usuniecia w 1.0
+#endif
+            Console.WriteLine("Wylosowalem liczbę, odgadnij ją!");
+            #endregion
+            int licznikruchow = 0;
+            bool trafiono = false;
+            do
+            {
+                #region propozycja
+                //2. gracz podaje propozyjce
 
-            string nazwa;
-            Console.Write("Napisz co: ");
-            nazwa = Console.ReadLine();
-            Console.WriteLine($"Napisales, {nazwa} bobie");
-            Console.WriteLine("Bye World");
+                Console.Write("Podaj twoją propozycję: ");
+                string tekst = Console.ReadLine();
+                //Console.WriteLine($"Podałeś tekst {tekst}");
+                int podana = Convert.ToInt32(tekst);
+                #endregion
+
+                #region ocenakomputera
+                //3. kumpoter ocenia
+                if (wylosowana < podana)
+                {
+                    Console.WriteLine("Za dużo :C");
+                }
+                else if (wylosowana > podana)
+                {
+                    Console.WriteLine("Za mało :C");
+                }
+                else
+                {
+                    Console.WriteLine("Trafiłeś!");
+                    trafiono = true;
+                }
+                #endregion
+                licznikruchow++;
+            }
+            while (!trafiono);
+            Console.WriteLine("Koniec gry");
+            Console.WriteLine($"Statystyki: Wykonane ruchy {licznikruchow}");
         }
     }
 }
